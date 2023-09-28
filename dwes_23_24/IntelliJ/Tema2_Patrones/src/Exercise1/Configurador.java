217b88eb4cd7296b66f6aa846f7b3635cd8e8110
dgonsk8 <dgongar3112@gmail.com>
2023-09-28 12:10:35 +0200

    28.09.23

---
+++ dwes_23_24/IntelliJ/Tema2_Patrones/src/Exercise1/Configurador.java
@@ -0,0 +1,15 @@
+package Exercise1;
+
+import java.util.ArrayList;
+
+public class Configurador {
+
+    private static Configurador instante = null;
+    private String configuracion;
+
+    private Configurador(String configuracion) {
+        this.configuracion = configuracion;
+    }
+
+    public static
+}
---
+++ dwes_23_24/IntelliJ/Tema2_Patrones/src/Exercise1/Main.java
@@ -0,0 +1,9 @@
+package Exercise1;
+
+public class Main {
+    public static void main(String[] args){
+
+        //Configurador config = Configurador
+
+    }
+}
